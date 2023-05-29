54475f707f385af22fa44f8599e5a48d41d935fc
PedroJoMa <pejomadejefi@gmail.com>
2023-05-28 23:49:26 -0300

    feat: função is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # Caso base: o index da palavra tem um caractere ou 2 caracteres iguais
+    if high_index == 1 or high_index == 0:
+        return True
+    # Caso base: os caracteres inicial e final são diferentes
+    elif not word or word[low_index] != word[-1]:
+        return False
+    # Chamada recursiva: verifica o palíndromo sem o primeiro e último caracter
+    else:
+        slice_word = word[1:-1]
+        return is_palindrome_recursive(slice_word, 0, len(word) - 1)
